84edd89ce57916a3cf4c02d288e9683853979dca
rslewenstein <rafasoaresjf@gmail.com>
2021-06-28 22:58:28 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <Calcula msg="Calcula Financiamento Veicular"/>
+</template>
+
+<script>
+import Calcula from './components/Calcula.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Calcula
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Calcula.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="calc">
+    <h1>{{ msg }}</h1>
+
+    <input type="text" v-model="valor_veiculo" v-bind:placeholder="valVeiculo">
+    <input type="text" v-model="valor_entrada" v-bind:placeholder="valEntrada">
+    <input type="text" v-model="valor_taxa_juros" v-bind:placeholder="valJuros">
+    <input type="button" value="Convert" v-on:click="convert()">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Calcula',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+/* ul {
+  list-style-type: none;
+  padding: 0;
+} */
+/* li {
+  display: inline-block;
+  margin: 0 10px;
+} */
+a {
+  color: #42b983;
+}
+</style>
